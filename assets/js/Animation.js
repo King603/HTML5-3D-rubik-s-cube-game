bc06405e4951121ed4afd1e53188e23d3dd0718f
King <408385413@.com>
2020-05-16 16:16:13 +0800

    备份

--- assets/js/Animation.js
+++ assets/js/Animation.js
@@ -27,7 +27,7 @@ let animationEngine = (() => {
     }
     /**
      * 添加
-     * @param {ObjectConstructor} animation 构造函数
+     * @param {{}} animation 构造函数
      */
     add(animation) {
       // 赋值于animation.id后自增
@@ -47,7 +47,7 @@ let animationEngine = (() => {
     // 
     /**
      * 删除
-     * @param {ObjectConstructor} animation 构造函数
+     * @param {{}} animation 构造函数
      */
     remove(animation) {
       // 获取animation.id在属性ids中的对应位置
